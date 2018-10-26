dcc7f788abbc0c5a4910c1f17ad88e89e384288e
Yuka <yukkuprog@gmail.com>
2018-10-26 20:06:00 +0900

    Updates

---
+++ precache-manifest.7ff064026ecaa1885b8dd7a50b8dd91a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "56b08f05f3e037da7bd55b2041db21de",
+    "url": "/starwars-app/static/media/universe-2218012_1920.56b08f05.jpg"
+  },
+  {
+    "revision": "90f48d1f1fa7d160d193",
+    "url": "/starwars-app/static/js/runtime~main.90f48d1f.js"
+  },
+  {
+    "revision": "b863403469d48dc61d64",
+    "url": "/starwars-app/static/js/main.b8634034.chunk.js"
+  },
+  {
+    "revision": "732423652facb224987e",
+    "url": "/starwars-app/static/js/1.73242365.chunk.js"
+  },
+  {
+    "revision": "b863403469d48dc61d64",
+    "url": "/starwars-app/static/css/main.67592245.chunk.css"
+  },
+  {
+    "revision": "732423652facb224987e",
+    "url": "/starwars-app/static/css/1.98991a9c.chunk.css"
+  },
+  {
+    "revision": "8f1e1cfbf47249ebfd55c7091a85d6c7",
+    "url": "/starwars-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/starwars-app/precache-manifest.7ff064026ecaa1885b8dd7a50b8dd91a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/starwars-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
